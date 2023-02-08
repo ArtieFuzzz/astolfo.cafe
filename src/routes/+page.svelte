9717e7f9d475c02663c9986e6bde150b07a1bfc3
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-02-08 20:36:41 +1100

    feat: An actual landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center">
-  <h1>Hello, World!</h1>
+  <h1 class="text-white text-5xl sm:text-7xl md:text-9xl">Astolfo.Cafe</h1>
 </div>
