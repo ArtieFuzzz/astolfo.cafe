53a31b833a061e68f22e9ad0214417242f003e3f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-02-09 11:44:10 +1100

    feat: Privacy Policy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center">
-  <h1 class="text-white text-5xl sm:text-7xl md:text-9xl">Astolfo.Cafe</h1>
+  <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
+  <a href="/privacy" class="text-2xl">Privacy Policy</a>
 </div>
+
+<style type="scss">
+  h1,
+  a {
+    color: white;
+  }
+</style>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,30 @@
+<div class="w-[60vw] flex flex-col justify-center items-center h-screen">
+  <div class="m-20">
+    <h1 class="text-white">Privacy Policy</h1>
+
+    <p>
+      This website uses Cloudflare, visit their <a href="https://www.cloudflare.com/privacypolicy/"
+        >Privacy Policy</a
+      >
+    </p>
+
+    <p>
+      This website does not store any data but this site uses Plausible Analytics for Analytical
+      purposes, using a self-hosted instance @ analytics.uvvu.pw. We do not track you in any way nor
+      do we sell your Information to Third-Parties.
+    </p>
+
+    <a
+      href="https://plausible.io/data-policy#first-thing-first-what-we-collect-and-what-we-use-it-for"
+      >- Plausible Analytics Data Collection Policy</a
+    >
+  </div>
+</div>
+
+<style type="scss">
+  p,
+  a {
+    color: white;
+    @apply text-2xl;
+  }
+</style>
