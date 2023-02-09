b0e3e0cb9de625b53fac234db70ea356efe0f81a
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-02-09 22:03:50 +1100

    refactor: Fitting image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="w-screen h-screen flex flex-row justify-center items-center">
-  <img src="astolfo.png" alt="Astolfo Best Boy" width=320 height=720 draggable=false/>
+  <img src="astolfo.png" alt="Astolfo Best Boy" width=620 height=1080 draggable=false/>
   <div class="flex flex-col justify-center items-center">
     <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
     <a href="/privacy" class="text-2xl">Privacy Policy</a>
