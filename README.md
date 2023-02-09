9c91a88d9e4d1e67aa38f8d31fe91bb91816677f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-02-09 20:08:47 +1100

    feat: Astolfo
    real

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Svelte Tempalte
+# astolfo.cafe
+
+> Astolfo's Cafe :3
+
+## Sources
+
+[Astolfo](https://www.deviantart.com/argaten166/art/Astolfo-Render-771036239)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
-<div class="w-screen h-screen flex flex-col justify-center items-center">
-  <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
-  <a href="/privacy" class="text-2xl">Privacy Policy</a>
+<div class="w-screen h-screen flex flex-row justify-center items-center">
+  <img src="astolfo.png" alt="Astolfo Best Boy" width=320 height=720 draggable=false/>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
+    <a href="/privacy" class="text-2xl">Privacy Policy</a>
+  </div>
 </div>
 
 <style type="scss">
   h1,
   a {
     color: white;
+  }
+
+  img {
+    padding: 0;
   }
 </style>
