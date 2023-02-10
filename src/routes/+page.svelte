908170e85c58914c02172356d625e62c5d9d2b87
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-02-10 12:20:24 +1100

    fix: Update png to webp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="w-screen h-screen flex flex-row justify-center items-center">
-  <img src="astolfo.png" alt="Astolfo Best Boy" width=620 height=1080 draggable=false/>
-  <div class="flex flex-col justify-center items-center">
+  <img src="astolfo.webp" alt="Astolfo Best Boy" width=620 height=1080 draggable=false/>
+  <div class="flex flex-col justify-center items-center mr-12">
     <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
     <a href="/privacy" class="text-2xl">Privacy Policy</a>
   </div>
