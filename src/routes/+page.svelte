03a3c8520887c0f8d27ca284f87a3c56b23c0746
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2023-11-27 13:23:28 +1100

    chore: Make astolfo image fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,23 @@
   import { slide } from 'svelte/transition';
 </script>
 
-<div class="w-screen h-screen flex flex-row justify-center items-center" transition:slide>
-  <img
+<div class="w-screen flex flex-row justify-center items-center" transition:slide>
+  <div class='w-screen h-screen flex justify-start max-md:hidden lg:visible'>
+    <img
     loading="lazy"
     src="astolfo.webp"
     alt="Astolfo"
-    class="max-sm:hidden lg:visible"
+    class="lg:fixed"
     width="620"
     height="1080"
     draggable="false"
   />
-  <div class="flex flex-col justify-center items-center md:mr-20">
-    <div class="flex flex-row items-baseline">
-      <h1 class="text-5xl sm:text-7xl md:text-9xl m-2">Astolfo.Cafe</h1>
-      <a href="https://dev.astolfo.cafe" class="text-md bold pr-2">| artsy.</a>
+  </div>
+
+  <div class="max-md:h-screen flex flex-col justify-center items-center md:mr-20">
+    <div class="flex flex-row place-items-baseline">
+      <h1 class="text-5xl sm:text-7xl md:text-9xl m-2 mb-0">Astolfo.Cafe</h1>
+      <a href="https://dev.astolfo.cafe" class="text-md bold">artsy.</a>
     </div>
     <div class="flex flex-row justify-start gap-4">
       <a href="/privacy" class="text-2xl">Privacy Policy</a>
