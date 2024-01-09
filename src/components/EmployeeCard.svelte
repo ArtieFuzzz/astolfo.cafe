48a0ab3a68c1ec47d973e493960c634b70c29248
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-09 21:39:28 +1100

    chore: Add `Employees` page

---
+++ src/components/EmployeeCard.svelte
@@ -0,0 +1,42 @@
+<script lang='ts'>
+
+  export let name: string
+  export let profilePicture: string
+  export let title: string
+</script>
+
+<div class='employee-card'>
+  <div class="flex justify-between items-center employee-meta">
+    <h2 class="font-heading text-3xl">{name}</h2>
+    <img src={profilePicture} alt="artsy" width="96" height="96" />
+  </div>
+
+  <div class="separator" />
+  <p class="font-heading text-2xl">{title}</p>
+</div>
+
+
+<style lang="scss">
+	:where(.employee-card) {
+		@apply flex flex-col justify-between items-center border-2 border-accent pb-3;
+		width: 20rem;
+		height: 16rem;
+
+		.employee-meta {
+			display: flex;
+			width: 21.875rem;
+
+			@apply px-10 py-5;
+		}
+
+		img {
+			@apply rounded-full border-2 border-accent;
+		}
+
+		.separator {
+			width: 3.125rem;
+			height: 0.625rem;
+			background-color: #ffffff;
+		}
+	}
+</style>
---
+++ src/routes/employees/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import EmployeeCard from '#components/EmployeeCard.svelte';
+
+	interface Employee {
+		name: string;
+		profilePicture: string;
+		title: string;
+	}
+
+	const employees: Employee[] = [
+		{ name: 'artsy.', profilePicture: '/profile/artsy.png', title: 'Owner & Developer' }
+	];
+</script>
+
+<div class="flex flex-col justify-start items-start w-screen max-w-full py-24 px-12 gap-20">
+	<div class="flex flex-col">
+		<h1 class="font-heading text-5xl">Astolfo’s Café</h1>
+		<p class="font-body text-2xl">
+			Our wonderful <span class="font-bold">Baristas</span> and
+			<span class="font-bold">Employees</span>!
+		</p>
+	</div>
+
+	<ul>
+		{#each employees as employee}
+			<ol>
+				<EmployeeCard
+					name={employee.name}
+					profilePicture={employee.profilePicture}
+					title={employee.title}
+				/>
+			</ol>
+		{/each}
+	</ul>
+</div>
+
+<style lang="scss">
+	:where(ul) {
+		@apply flex max-w-full justify-center items-center gap-8 self-stretch flex-wrap content-center;
+	}
+</style>
