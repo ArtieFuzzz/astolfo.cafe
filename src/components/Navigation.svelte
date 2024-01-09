45176e51cafbc5480b2a46bfde1af3bc31e175d6
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-09 18:02:03 +1100

    chore: Update `Navigation` component

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,13 +1,23 @@
+<script>
+	import { page } from '$app/stores';
+
+	const home = $page.url.href === '/';
+</script>
+
 <header>
 	<nav class="flex flex-row justify-between w-screen p-6">
 		<p class="text-2xl">astolfo.café <span class="text-base">&copy;</span></p>
 
-		<div class="flex flex-row gap-5">
-			<a href="mailto:hey@astolfo.cafe">Contact</a>
-			<a href="#menu">Menu</a>
-			<a href="#employees">Employees</a>
-			<a href="#data-policy">Privacy Policy</a>
-		</div>
+		{#if home}
+			<div class="flex flex-row gap-5">
+				<a href="mailto:hey@astolfo.cafe">Contact</a>
+				<a href="#menu">Menu</a>
+				<a href="#employees">Employees</a>
+				<a href="#data-policy">Privacy Policy</a>
+			</div>
+		{:else}
+			<a href="/">Home</a>
+		{/if}
 	</nav>
 </header>
 
