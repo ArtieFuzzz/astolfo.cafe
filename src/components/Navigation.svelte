3b3e647e8dcfa6a76244dca6396650e8f81b20ef
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-09 17:10:13 +1100

    refactor: Create `Navigation` component

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,17 @@
+<header>
+  <nav class="flex flex-row justify-between w-screen p-6">
+    <p class='text-2xl'>astolfo.café <span class='text-xl'>&copy;</span></p>
+
+    <div class='flex flex-row gap-5'>
+      <a href="#menu">Menu</a>
+      <a href="#employees">Employees</a>
+      <a href="#data-policy">Privacy Policy</a>
+    </div>
+  </nav>
+</header>
+
+<style lang='scss'>
+  a {
+    @apply text-text text-xl font-heading;
+  }
+</style>
