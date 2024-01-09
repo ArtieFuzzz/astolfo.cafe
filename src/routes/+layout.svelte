61e0d31d6f78522eaceaeb8175f733e31662df08
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-09 17:15:34 +1100

    refactor: Update layout
    Adapted from Figma dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,13 @@
   import Navigation from '#components/Navigation.svelte';
 </script>
 
-<div class="max-h-screen overflow-hidden">
+<div class="max-h-screen flex flex-col justify-center" id='main'>
   <Navigation />
 	<slot />
 </div>
+
+<style>
+  #main {
+    align-items: flex-start;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-screen h-[80dvh] justify-center items-center">
+<div class="flex flex-col w-screen h-screen justify-center items-center">
 	<div class="flex flex-col">
 		<h1 class="font-heading text-5xl">Astolfo’s Café</h1>
 		<p class="font-body text-2xl">Your Local <span class="font-bold">Femboy</span> café!</p>
