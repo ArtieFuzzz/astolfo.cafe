d17c7fd06700e528ec6b8d7a5bac7ee014109c51
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-09 17:04:42 +1100

    refactor: Updated Front page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '#styles/main.scss';
+</script>
+
+<div class="max-h-screen overflow-hidden">
+	<header>
+		<nav class="flex flex-row justify-between w-screen p-6">
+      <p class='text-2xl'>astolfo.café <span class='text-xl'>&copy;</span></p>
+
+      <div class='flex flex-row gap-5'>
+        <a href="#menu">Menu</a>
+        <a href="#employees">Employees</a>
+        <a href="#data-policy">Privacy Policy</a>
+      </div>
+    </nav>
+	</header>
+	<slot />
+</div>
+
+<style lang='scss'>
+  a {
+    @apply text-text text-xl font-heading;
+  }
+</style>
