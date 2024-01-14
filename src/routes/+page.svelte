f072af54f67ad50bcc1eb377d742a3cfbc0c5211
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-14 12:01:35 +1100

    chore: Add menu section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
-<div class="flex flex-col w-screen h-[90vh] justify-center items-center">
-  <div class="flex flex-col">
+<div class="max-w-full flex flex-col w-screen justify-center items-center">
+  <div class="flex flex-col justify-center h-screen">
     <h1 class="font-heading text-5xl">Astolfo’s Café</h1>
     <p class="font-body text-2xl">Your Local <span class="font-bold">Femboy</span> café!</p>
+  </div>
+  <div class="max-w-full h-screen w-screen justify-center flex flex-col items-center gap-28">
+    <div class="px-24 pb-8 border-b-[5px] border-accent">
+      <h2 class="font-heading text-4xl">Menu</h2>
+    </div>
   </div>
 </div>
