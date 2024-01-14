0d30066bde2da1236f1ae9ec53d362bb5b725d04
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-01-14 12:12:46 +1100

    fix: Menu button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <h1 class="font-heading text-5xl">Astolfo’s Café</h1>
     <p class="font-body text-2xl">Your Local <span class="font-bold">Femboy</span> café!</p>
   </div>
-  <div class="max-w-full h-screen w-screen justify-center flex flex-col items-center gap-28">
+  <div class="max-w-full h-screen w-screen justify-center flex flex-col items-center gap-28" id='menu'>
     <div class="px-24 pb-8 border-b-[5px] border-accent">
       <h2 class="font-heading text-4xl">Menu</h2>
     </div>
