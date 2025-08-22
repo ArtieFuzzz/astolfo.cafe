a3b77ffc09e33d1f59f551f8900ddc19f51e8c03
ArtieFuzzz <hey@astolfo.cafe>
2025-08-22 17:40:09 +1000

    Remove subtitle text
    I'll be updating this later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="max-w-full flex flex-col w-screen justify-center items-center">
   <div class="flex flex-col justify-center h-screen">
     <h1 class="font-heading text-5xl">Astolfo’s Café</h1>
-    <p class="font-body text-2xl">Your Local <span class="font-bold">Femboy</span> café!</p>
+    <p class="font-body text-2xl">Your Local café!</p>
   </div>
   <div
     class="max-w-full h-screen w-screen justify-center flex flex-col items-center gap-28"
